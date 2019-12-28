fe827a594bc257af643a63c230f81016a508207f
nezter <nportello21@gmail.com>
2019-12-28 04:01:05 -0500

    removed the image in the admin portal

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -18,9 +18,7 @@ export default class PostPreview extends React.Component {
 
         <div className="bg-grey-1 pv4">
           <div className="flex-l mhn1-l ph3 center mw7">
-            <h2 className="f2 b lh-title mb2 w-40-l">{entry.getIn(["data", "blurb", "heading"])}
-    <img src="/img/caddy-ed-with-text.png" alt="Caddy Ed logo" style="/* margin-top: -195px; */" class="db w6 center pv0 br-100">
-</h2>
+            <h2 className="f2 b lh-title mb2 w-40-l">{entry.getIn(["data", "blurb", "heading"])}</h2>
             <p className="w-60-l mb0">{entry.getIn(["data", "blurb", "text"])}</p>
           </div>
         </div>
