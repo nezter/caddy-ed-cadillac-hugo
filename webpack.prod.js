cb7335f6b5c2f9d4d68eb4c7d72361e0211ac343
Nick Portello <nport3llo21@gmail.com>
2025-03-02 23:25:04 -0500

    Enhance production build configuration: add optimization settings for JavaScript and CSS, including Terser and MiniCssExtract plugins

--- webpack.prod.js
+++ webpack.prod.js
@@ -55,6 +55,44 @@ module.exports = merge(common, {
     minimizer: [
       new TerserPlugin({
         parallel: true,
+        exclude: /\/node_modules\//,
+        terserOptions: {
+          compress: {
+            drop_console: true
+          },
+          mangle: true
+        }
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash:5].css",
+      chunkFilename: "[id].[hash:5].css"
+    })
+  ]
+});
+const { merge } = require("webpack-merge");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+
+  output: {
+    filename: "[name].[hash:5].js",
+    chunkFilename: "[id].[hash:5].css"
+  },
+
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
         terserOptions: {
           compress: {
             drop_console: true
